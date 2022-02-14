a1afa83b86782316dafc5b782ef6c814c745ab1e
Kohei <yamamoto.kohei@ais.cmc.osaka-u.ac.jp>
2022-02-14 10:47:40 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port].");
+        std::process::exit(1);
+    }
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address; &str = &args[3];
+    match protocol {
+        "tcp" => match role {
+            "server" => {
+                // TO DO: TCP サーバの呼び出し
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
+            }
+            "client" => {
+                // TO DO 
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                // TO DO: UDP サーバの呼び出し
+            }
+            "client" => {
+                // TO DO 
+            }
+            _ => {
+                missing_role();
+            }
+        }, 
+        _ => {
+            error!("Please specify tcp or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
+    println!("Hello, world!");
+}
+//! #　missing_role
+//! 
+//! 第二引数が不正な時にエラーを出す関数
+//! 
+//! 
+fn missing_role(){
+    error!("Please specify server or client on the 2nd argument. ");
+    std::process::exit(1);
+}
